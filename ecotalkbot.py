0d892c049424abca022e1437abbf9fbd02d426e2
hireck <reckmanhgb@gmail.com>
2025-02-05 14:14:30 +0100

    updated description of where to find list of sources

--- ecotalkbot.py
+++ ecotalkbot.py
@@ -26,6 +26,7 @@ import weaviate.classes.query as wq
 from weaviate.classes.query import Filter
 from FlagEmbedding import BGEM3FlagModel
 from streamlit_float import *
+#from streamlit_extras.bottom_container import bottom
 
 float_init(theme=True, include_unstable_primary=False)
 
@@ -99,13 +100,13 @@ openai.api_key = apikey
 
 with st.sidebar:
     expander = st.expander("Hvad er EcoTalkBot?")
-    expander.write("EcoTalkBot er en chatbot udviklet som en del af et forskningsprojekt på Aarhus Universitet, der har til formål at fremme interessen og forståelsen for biodiversitet i landbrugslandet. Gennem en interaktiv dialog med chatbotten har du nem adgang til den nyeste viden omkring biodiversitet. Svarene er baseret på et udvalg af pålidelige kilder, som henvises direkte til i svarene. Denne gennemsigtighed er med til at understøtte en let tilgængelig, oplyst dialog omkring biodiversitet i landbruget.")
+    expander.write("EcoTalkBot er en chatbot udviklet som en del af et forskningsprojekt på Aarhus Universitet, der har til formål at fremme interessen og forståelsen for biodiversitet i landbrugslandet. Gennem en interaktiv dialog med chatbotten har du nem adgang til den nyeste viden omkring biodiversitet. Svarene er baseret på et udvalg af pålidelige kilder, som henvises direkte til i svarene. For at se den liste over alle de pålidelige kilder, vi aktuelt bruger til dette projekt, kan du klikke på fanen 'Kildeliste', som findes ved siden af chatbot-fanen midt på skærmen. Denne gennemsigtighed er med til at understøtte en let tilgængelig, oplyst dialog omkring biodiversitet i landbruget.")
     expander = st.expander("Sådan bruger du EcoTalkBot")
     expander.write("For at starte en dialog skal du blot skrive dit spørgsmål i skrivefeltet i bunden af skærmen. EcoTalBot er i øjeblikket i en testfase og indsamler data fra brugerinteraktioner som en del af projektet. Vi bruger udelukkende jeres samtaler til videnskabelige analyser i forbindelse med vores forskning.")
     expander = st.expander("GDPR og datasikkerhed")
     expander.write("Samtaledata og demografiske oplysninger vil blive pseudonymiseret for at sikre GDPR-overholdelse og vil blive behandlet fortroligt. Chatbotten fra universitetet registrerer dine svar, herunder eventuelle personlige oplysninger, som du måtte vælge at dele. For at minimere risikoen opfordrer vi dig til at undgå at inkludere personhenførbare oplysninger i samtalen. Vi opbevarer data sikkert på en krypteret server hos Aarhus Universitet og bruger kun de indsamlede data til videnskabelige, ikke-kommercielle formål, herunder potentielle publikationer og præsentationer. Data fra undersøgelsen opbevares i op til 5 år og slettes derefter sikkert. Alle dataindsamlinger og -behandlinger overholder EU's generelle databeskyttelsesforordning (GDPR) 2016/679. Vi bruger kun dine ikke-følsomme, pseudonymiserede data til forskningsformål. Hvis du har spørgsmål om, hvordan dine data opbevares eller behandles, kan du kontakte Aarhus Universitets databeskyttelsesrådgiver (DPO): Søren Broberg Nielsen via e-mail: soren.broberg@au.dk. Aarhus Universitet, CVR nr. 31119103, er dataansvarlig for behandlingen af dine data.")
     expander = st.expander("Åbenhed og gennemsigtighed omkring vores brug af GenAI")
-    expander.write("Denne chatbot er baseret på et udvalg af pålidelige kilder og på sprogmodellen GPT-4o fra OpenAI, som er et Generative Artificial Intelligence (GenAI) værktøj og tilgås gennem Microsoft Azure for dette projekt. Som med enhver GenAI bedes du undlade at dele oplysninger, der involverer forretningshemmeligheder, fortrolige eller følsomme data eller ophavsretligt beskyttet materiale.  \n\n  De svar, du modtager, er derfor skabt af GenAI baseret på et udvalg af pålidelige kilder og generel information indbygget i sprogmodellen gennem fortræning på store mængder text af forskellige slags. Alle svar genereres automatisk og kan indeholde fejl. For at se den aktuelle liste over de pålidelige kilder, vi bruger til dette projekt, kan du klikke på fanen 'Kildeliste'.")
+    expander.write("Denne chatbot er baseret på et udvalg af pålidelige kilder og på sprogmodellen GPT-4o fra OpenAI, som er et Generative Artificial Intelligence (GenAI) værktøj og tilgås gennem Microsoft Azure for dette projekt. Som med enhver GenAI bedes du undlade at dele oplysninger, der involverer forretningshemmeligheder, fortrolige eller følsomme data eller ophavsretligt beskyttet materiale.  \n\n  De svar, du modtager, er derfor skabt af GenAI baseret på et udvalg af pålidelige kilder og generel information indbygget i sprogmodellen gennem fortræning på store mængder text af forskellige slags. Alle svar genereres automatisk og kan indeholde fejl.")
     expander = st.expander("Finansiering")
     expander.write("EcoTalkBot er en del af projektet EcoMetric, som har til formål at udvikle rammerne for et biodiversitetsmål, som kan bruges i forvaltningen til at fremme biodiversitet i landbrugslandskaber. EcoTalkBot er finansieret af seed funding fra DIGIT (Centre for Digitalisation, Big Data and Data Analytics), Aarhus Universitet. Yderligere finansiering er ydet af Ministeriet for Fødevarer, Landbrug og Fiskeri gennem Organic RDD9, koordineret af ICROFS (Internationalt Center for Forskning i Økologiske Fødevaresystemer) med støtte fra Grønt Udviklings- og Demonstrationsprogram (GUDP). Læs mere om projektet her https://projects.au.dk/sess/projects/ecometric")
     expander = st.expander("Kontakt os")
@@ -367,12 +368,13 @@ with tab1:
     container = st.container()
     container.float(css=float_css_helper(width="2.2rem", bottom="1rem", transition=0))
     with container:
+    #with bottom:
         st.chat_input(key='content')
         #float_parent(css=float_css_helper(width="2.2rem", bottom="1rem", transition=0))
     #with st.container():
     if content:=st.session_state.content:
         user_input = str(copy.deepcopy(content))
-        del st.session_state['content']
+        #del st.session_state['content']
     #if user_input := st.chat_input():   
         #st.write(f"Welcome, {st.session_state['username']}!")
         print(user_input)
